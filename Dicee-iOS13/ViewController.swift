49843e4c975775932f1c8e6157f41a4cbe642a90
muhammadshams <mr.shamssheikh@gmail.com>
2020-02-15 19:58:12 +0500

    Complete app with responsive design

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgOne: UIImageView!
+    @IBOutlet weak var imgTwo: UIImageView!
+    
+    var arry = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func ChangePicture(_ sender: Any) {
+         
+        let _maxLength: Int  = arry.count
+        
+        imgOne.image = arry[Int.random(in : 0..<_maxLength)]
+        imgTwo.image = arry[Int.random(in : 0..<_maxLength)]
+        
+    }
 }
 
